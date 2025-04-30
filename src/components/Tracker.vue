7f358686f7eb0d7f4454709f8718d98387d3e1ac
yaasir cheekoory <yaasir1997@gmail.com>
2025-04-30 18:08:50 +0400

    add tailwind

--- src/components/Tracker.vue
+++ src/components/Tracker.vue
@@ -131,17 +131,6 @@ function loadEntries() {
 onMounted(() => {
   loadEntries();
 });
-
-// Register service worker for PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
-      console.log('ServiceWorker registration successful');
-    }).catch(error => {
-      console.log('ServiceWorker registration failed:', error);
-    });
-  });
-}
 </script>
 
 <template>
